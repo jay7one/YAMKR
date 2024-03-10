81c7aed085cd86684d2929b6d3631827e2aaea8a
jay7one <89343983+jay7one@users.noreply.github.com>
2024-03-10 11:26:17 +0000

    removed unused

--- src/not_used/app_window.py
+++
@@ -1,9 +0,0 @@
-from tkinter import Tk
-class AppWindow(Tk):
-    def __init__(self, name, w:int, h:int):
-        super().__init__()
-        self.title(name)
-        ws,hs = self.winfo_screenwidth(), self.winfo_screenheight()
-        x,y = int((ws / 2) - (w / 2)), int((hs / 2) - (h / 2))
-        self.geometry(f'{w}x{h}+{x}+{y}')
-        self.resizable(False, False)
